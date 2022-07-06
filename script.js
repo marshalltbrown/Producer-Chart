980df42b3b683155f2bf0d6969ad778fbebc1a49
Marshall Brown <82731225+marshalltbrown@users.noreply.github.com>
2022-07-06 17:48:49 +0000

    General Stacking Structure created and functioning.

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
+const chart_data = {
+	labels: ['Ariana', 'Cindy', 'Jacob', 'Lauren', 'Marshall', 'Zach'],
+	datasets: [{
+		label: 'Personal Lines',
+		data: [100, 20],
+		parsing: {
+			yAxisKey: 'pl'
+		}
+	}, {
+		label: 'Commercial',
+		data: [50, 10],
+		parsing: {
+			yAxisKey: 'com'
+		}
+	}, {
+		label: 'Life',
+		data: [50, 10],
+		parsing: {
+			yAxisKey: 'life'
+		}
+	}, {
+		label: 'Other',
+		data: [100, 500], 
+		parsing: {
+			yAxisKey: 'other'
+		}
+	}]
+}
+
+
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+  type: 'bar',
+  data: chart_data,
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart - Stacked'
+      },
     },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
+    responsive: true,
+    scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true
+      }
     }
+  }
 });
